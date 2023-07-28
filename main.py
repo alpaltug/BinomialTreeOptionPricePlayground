ce8d779757512d037b76c4e6714607c179bf4e37
Alp Altug <alpaltug@berkeley.edu>
2023-07-28 11:28:27 -0500

    added volatility smile concept with distinct implied volatilities for distinct options

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ r = 0.05      # risk free rate
 v = 0.2       # volatility
 N = 1000      # number of steps
 
+implied_volatilities = []
+atm_implied_volatility = 0.20
+increment_per_step = 0.001
+for i in range(N + 1):
+    strike_distance = i - N / 2  # Distance from the ATM strike
+    implied_vol = atm_implied_volatility + strike_distance * increment_per_step
+    implied_volatilities.append(implied_vol)
+
 option_type = 'call'
 
 # Precompute constants
@@ -23,16 +31,20 @@ time_to_maturity_range = np.linspace(0.1, 1, 10)
 volatility_range = np.linspace(0.1, 0.5, 10)
 
 
-def binomial_tree(S0, K, T, r, v, N, option_type='call', dividend=0.0, div_dates=[], div_amounts=[]):
+def binomial_tree(S0, K, T, r, implied_volatilities, N, option_type='call', dividend=0.0, div_dates=[], div_amounts=[]):
     dt = T / N
-    u = math.exp((v * math.sqrt(dt)))  # Up factor
-    d = 1 / u  # Down factor
+    p_values = []  # To store the risk-neutral probabilities
 
-    # Calculate risk-neutral probability without dividends
-    p = (math.exp(r * dt) - d) / (u - d)
+    # Calculate the up and down factors for each time step based on the implied volatilities
+    up_factors = [math.exp(implied_vol * math.sqrt(dt)) for implied_vol in implied_volatilities]
+    down_factors = [1 / up_factor for up_factor in up_factors]
+
+    # Calculate the risk-neutral probabilities for each time step using the up and down factors
+    for i in range(N):
+        p_values.append((math.exp(r * dt) - down_factors[i]) / (up_factors[i] - down_factors[i]))
 
     # Calculate option prices at the end nodes of the binomial tree
-    end_nodes = [S0 * u**j * d**(N - j) for j in range(N + 1)]
+    end_nodes = [S0 * up_factors[N - j] * down_factors[j] for j in range(N + 1)]
     if option_type == 'call':
         option_prices = [max(0, S - K) for S in end_nodes]
     else:
@@ -46,13 +58,13 @@ def binomial_tree(S0, K, T, r, v, N, option_type='call', dividend=0.0, div_dates
                 for div_date, div_amount in zip(div_dates, div_amounts):
                     if div_date * T <= j * dt:
                         div_adjustment *= (1 - div_amount / (end_nodes[j] * (1 - dividend)))  # Adjusted stock price
-                option_prices[j] = (p * option_prices[j + 1] + (1 - p) * option_prices[j]) * math.exp(-r * dt) * div_adjustment
+                option_prices[j] = (p_values[i] * option_prices[j + 1] + (1 - p_values[i]) * option_prices[j]) * math.exp(-r * dt) * div_adjustment
 
     else:
         # Calculate option prices at earlier nodes using backward induction
         for i in range(N - 1, -1, -1):
             for j in range(i + 1):
-                option_prices[j] = (p * option_prices[j + 1] + (1 - p) * option_prices[j]) * math.exp(-r * dt)
+                option_prices[j] = (p_values[i] * option_prices[j + 1] + (1 - p_values[i]) * option_prices[j]) * math.exp(-r * dt)
 
     return option_prices[0]
 
@@ -132,8 +144,9 @@ def calculate_implied_volatility(stock_price, strike_price, risk_free_rate, opti
 def plot_delta_vs_theta(stock_price, strike_price, risk_free_rate, volatility_range, time_to_maturity, option_type):
     deltas = []
     thetas = []
+    N = 1000
     for v in volatility_range:
-        option_price = binomial_tree(stock_price, strike_price, time_to_maturity, risk_free_rate, v, N, option_type)
+        option_price = binomial_tree(stock_price, strike_price, time_to_maturity, risk_free_rate, implied_volatilities, v, N, option_type)
         delta = calculate_delta(stock_price, strike_price, risk_free_rate, v, time_to_maturity, option_type)
         theta = calculate_theta(stock_price, strike_price, risk_free_rate, v, time_to_maturity, option_price, option_type)
         deltas.append(delta)
@@ -147,8 +160,9 @@ def plot_delta_vs_theta(stock_price, strike_price, risk_free_rate, volatility_ra
 def plot_theta_vs_vega(stock_price, strike_price, risk_free_rate, volatility_range, time_to_maturity, option_type):
     thetas = []
     vegas = []
+    N = 1000
     for v in volatility_range:
-        option_price = binomial_tree(stock_price, strike_price, time_to_maturity, risk_free_rate, v, N, option_type)
+        option_price = binomial_tree(stock_price, strike_price, time_to_maturity, risk_free_rate, implied_volatilities, v, N, option_type)
         theta = calculate_theta(stock_price, strike_price, risk_free_rate, v, time_to_maturity, option_price, option_type)
         vega = calculate_vega(stock_price, strike_price, risk_free_rate, v, time_to_maturity)
         thetas.append(theta)
@@ -160,8 +174,8 @@ def plot_theta_vs_vega(stock_price, strike_price, risk_free_rate, volatility_ran
     plt.show()
 
 def main():
-    call_price = binomial_tree(S0, K, T, r, v, N, option_type='call')
-    put_price = binomial_tree(S0, K, T, r, v, N, option_type='put')
+    call_price = binomial_tree(S0=100, K=105, T=1, r=0.05, implied_volatilities=implied_volatilities, N=100, option_type='call', dividend=0.03, div_dates=[0.5], div_amounts=[2.0])
+    put_price = binomial_tree(S0=100, K=105, T=1, r=0.05, implied_volatilities=implied_volatilities, N=100, option_type='put', dividend=0.03, div_dates=[0.5], div_amounts=[2.0])
 
     d1 = calculate_d1(S0, K, r, v, T)
     d2 = calculate_d2(S0, K, r, v, T)
@@ -180,7 +194,7 @@ def main():
     rho = calculate_rho(S0, K, r, v, T, option_type)
     implied_volatility = calculate_implied_volatility(S0, K, r, option_price, T, option_type)
 
-    option_price = binomial_tree(S0=100, K=105, T=1, r=0.05, v=0.2, N=100, option_type='call', dividend=0.03, div_dates=[0.5], div_amounts=[2.0])
+    option_price = binomial_tree(S0=100, K=105, T=1, r=0.05, implied_volatilities=implied_volatilities, N=100, option_type='call', dividend=0.03, div_dates=[0.5], div_amounts=[2.0])
     print("European call with dividents: ", option_price)
 
     print("European call option price: ", round(call_price, 2))
@@ -193,8 +207,8 @@ def main():
     print("European", option_type, "option rho value:", rho)
     print("European", option_type, "option implied volatility value:", implied_volatility)
 
-    plot_delta_vs_theta(S0, K, r, volatility_range, T, option_type)
-    plot_theta_vs_vega(S0, K, r, volatility_range, T, option_type)
+    #plot_delta_vs_theta(S0, K, r, volatility_range, T, option_type)
+    #plot_theta_vs_vega(S0, K, r, volatility_range, T, option_type)
 
 if __name__ == "__main__":
     main()
